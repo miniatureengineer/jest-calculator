3c1d28c6d44576a099051ec757129d38ba49f097
sy <sy@example.com>
2021-11-14 07:40:45 +0800

    Updated __tests__

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -7,23 +7,23 @@ const b = 2;
 // 5. Describe test cases
 describe("Test basic calculator", () => {
 
-    // 6. ("Describe what you want to test for add")
-    test("add a + b = 6", () => {
+    // 6. ("Describe what you want to test for add.")
+    test("add 4 + 2 = 6", () => {
         //7. Expect result
         expect(calculator.add(a, b)).toBe(2);
     });
     
-    // ("Describe what you want to test for subtract")
-    test("subtract a - b = 2", () => {
+    // ("Describe what you want to test for subtract.")
+    test("subtract 4 - 2 = 2", () => {
         expect(calculator.subtract(a, b)).toBe(1);
     });
 
-    // ("Describe what you want to test for multiply")
+    // ("Describe what you want to test for multiply.")
     test("multiply 2 * 4 = 8", () => {
         expect(calculator.multiply(a, b)).toBe(4);
     });
 
-    // ("Describe what you want to test for divide")
+    // ("Describe what you want to test for divide.")
     test("divide 4 / 2 = 2", () => {
        expect(calculator.divide(a, b)).toBe(1);
     });
